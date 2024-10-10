f88c851bdac5e581f8e7faa07c9bd15fd7b84ff6
j.kibisz-aleszczyk <j.kibisz-aleszczyk@godeltech.com>
2024-10-10 12:58:39 +0200

    7

--- index.js
+++ index.js
@@ -22,8 +22,34 @@ app.get('/asteroids', async (req, res) => {
   }
 });
 
+app.get('/meteors', async (req, res) => {
+  try {
+    const asteroidData = await fetchData.fetchAsteroidData();
+    const filteredAsteroidData = Object.values(asteroidData)
+      .flat()
+      .map((asteroid) => {
+        return {
+          id: asteroid.id,
+          name: asteroid.name,
+          diameter_in_meters: asteroid.estimated_diameter.meters,
+          is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+          close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+          relative_velocity_kilometers_per_second: asteroid.close_approach_data[0].relative_velocity
+              .kilometers_per_second
+        };
+      });
+
+    // Send JSON response to the client
+    res.json({
+      message: 'Asteroids observed from Monday to today',
+      data: filteredAsteroidData,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
