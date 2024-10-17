a6b48b3d5d497567f5d24454a785922df33733c7
j.kibisz-aleszczyk <j.kibisz-aleszczyk@godeltech.com>
2024-10-17 10:38:02 +0200

    Task-13 Adding settings for form in index file and Form for rover photo.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import errorHandler from './error_handler/errorMidleware.js';
 const app = express();
 const PORT = config.port;
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure('views', {
   autoescape: true,
