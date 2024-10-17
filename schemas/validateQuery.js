8830921de7fbfb6d9f82396c1de0fcf875435313
j.kibisz-aleszczyk <j.kibisz-aleszczyk@godeltech.com>
2024-10-17 10:06:13 +0200

    Task-12 Added Joi and checked all parameters of all requests.

---
+++ schemas/validateQuery.js
@@ -0,0 +1,10 @@
+// import { meteorQuerySchema } from "../schemas/meteorSchema.js";
+
+// function validateMeteorQuerySchema(query) {
+//   const { error } = meteorQuerySchema.validate(query);
+//   if (error) {
+//     return res.status(400).json({ error: error.details[0].message });
+//   }
+// };
+
+// export default validateMeteorQuerySchema;
